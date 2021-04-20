d4c8e91e228647f6ad70f977fcfd56d7f236181c
silveiraSoft <adalbertosn1982@gmail.com>
2021-04-20 06:04:59 -0300

    Meu primiero commit do projeto base

---
+++ src/main/java/com/adalbertosn/demo/GazinApiV2Application.java
@@ -0,0 +1,14 @@
+package com.adalbertosn.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GazinApiV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GazinApiV2Application.class, args);
+		System.out.println("Hello");
+	}
+
+}
---
+++ src/test/java/com/adalbertosn/demo/GazinApiV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adalbertosn.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GazinApiV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
